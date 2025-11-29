70734a37bd31c1781003656515fb94b975647d96
Manoj <123manojverma2005@gmail.com>
2025-11-30 00:33:30 +0530

    simple values

---
+++ Day1/2_simple_values/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	// simple values
+	// int
+	fmt.Println(1+1)
+
+	// string
+	fmt.Println("hello golang")
+	// bool
+	fmt.Println(true)
+
+	// floats
+	fmt.Println(10.5/3)
+
+	
+}

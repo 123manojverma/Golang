209cf06125c07149119e4ddf8c05e13a334a93b0
Manoj <123manojverma2005@gmail.com>
2025-11-30 00:38:38 +0530

    variables

---
+++ Day1/3_variables/variables.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main(){
+	// var name string="golang"
+	
+	// infer type
+	// var name="golang"
+
+	// var isAdult bool=true
+
+	// var age int = 30
+
+	// shortend syntax
+	// name:="golang"
+
+	// var name string
+	// name="golang"
+
+	var price float32 = 50.5
+
+	fmt.Println(price)
+
+}

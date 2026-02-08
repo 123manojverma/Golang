6f71ad1b8ecc3198aff20c2b84f97dfd7f22437e
Manoj <123manojverma2005@gmail.com>
2026-02-08 22:33:19 +0530

    interfaces

---
+++ Day3/17_interface/Interface.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+
+type paymenter interface{
+	pay(amount float32)
+	refund(amount float32,account string)
+}
+
+type payment struct{
+	gateway paymenter
+}
+
+// Open close principle
+func(p payment) makePayment(amount float32){
+	// razorpayPaymentGw:=razorpay{}
+	// stripePaymentGw:=stripe{}
+	// razorpayPaymentGw.pay(amount)
+	p.gateway.pay(amount)
+}
+
+func(p payment) getrefund(amount float32,account string){
+	p.gateway.refund(amount,account)
+}
+
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32){
+	// logic to make payment
+	fmt.Println("Making payment using razorpay",amount)
+}
+
+// type stripe struct{}
+
+// func (s stripe) pay(amount float32){
+// 	fmt.Println("Making payment using stripe",amount)
+// }
+
+type fakepayment struct{}
+
+func (f fakepayment) pay(amount float32){
+	fmt.Println("Making payment using fake gateway for testing purpose")
+}
+
+type paypal struct{}
+
+func (p paypal) pay(amount float32){
+	fmt.Println("Making payment using paypal",amount)
+}
+
+func (p paypal) refund(amount float32,account string){
+	fmt.Println("Refund",amount,"credited to",account)
+}
+
+func main(){
+	// stripePaymentGw:=stripe{}
+	// razorpayPaymentGw:=razorpay{}
+	// fakeGw:=fakepayment{}
+	paypalGw:=paypal{}
+	newPayment:=payment{
+		gateway: paypalGw,
+	}
+	newPayment.makePayment(100)
+	newPayment.getrefund(1000,"123456")
+}
